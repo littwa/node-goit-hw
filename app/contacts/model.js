501ea86ebaca8ed8ebcc176fc7a95abecd263262
littwa <littwa777@gmail.com>
2020-10-13 09:05:28 +0300

    03-complited

--- app/contacts/model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const ContactsSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-});
-
-module.exports = model("Contacts", ContactsSchema);
